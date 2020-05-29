ffb72af43682ded5a3c710416832aac2f83b9b79
JJ Allaire <jj@rstudio.com>
2020-05-29 16:14:28 -0400

    make version a parameter

--- download.R
+++ download.R
@@ -2,4 +2,6 @@
 
 library(rmarkdown)
 
-render("download.Rmd", output_file = "docs/_download.md")
+render("download.Rmd", output_file = "docs/_download.md", params = list(
+  version = "1.4.411"
+))
